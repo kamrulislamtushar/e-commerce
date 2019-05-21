5263a232a4abc139ae9510de0160b536d4e43ca9
Kamrul Islam Tushar <tushar@augnitive.com>
2019-05-22 00:08:08 +0600

    details page

--- packages/Webkul/Shop/src/Resources/views/products/view.blade.php
+++ packages/Webkul/Shop/src/Resources/views/products/view.blade.php
@@ -15,101 +15,108 @@
     {!! view_render_event('bagisto.shop.products.view.before', ['product' => $product]) !!}
 
     <section class="product-detail">
-
         <div class="layouter">
-            <product-view>
-                <div class="form-container">
-                    @csrf()
+            <div class="row">
+                <product-view>
+                    <div class="form-container">
+                        @csrf()
 
-                    <input type="hidden" name="product" value="{{ $product->id }}">
+                        <input type="hidden" name="product" value="{{ $product->id }}">
 
-                    @include ('shop::products.view.gallery')
+                        @include ('shop::products.view.gallery')
 
-                    <div class="details">
+                        <div class="details">
 
-                        <div class="product-heading">
-                            <span>{{ $product->name }}</span>
-                        </div>
+                            <div class="row">
+                                <div class="col-6 col-sm-6 col-md-6">
+                                    <div class="product-heading">
+                                        <span>{{ $product->name }}</span>
+                                    </div>
 
-                        @include ('shop::products.review', ['product' => $product])
+                                    @include ('shop::products.review', ['product' => $product])
 
-                        @include ('shop::products.price', ['product' => $product])
+                                    @include ('shop::products.price', ['product' => $product])
 
-                        @include ('shop::products.view.stock', ['product' => $product])
-                        <div class="row">
-                            <div class="col-2">
-                                <div class="quantity control-group" :class="[errors.has('quantity') ? 'has-error' : '']">
+                                    @include ('shop::products.view.stock', ['product' => $product])
+                                </div>
+                                <div class="col-6 col-sm-6 col-md-6">
+                                    @if ($product->type == 'configurable')
+                                        <input type="hidden" value="true" name="is_configurable">
+                                    @else
+                                        <input type="hidden" value="false" name="is_configurable">
+                                    @endif
 
-                                    <label class="required">{{ __('shop::app.products.quantity') }}</label>
-
-                                    <input name="quantity" class="control" value="1" v-validate="'required|numeric|min_value:1'" style="width: 60px;" data-vv-as="&quot;{{ __('shop::app.products.quantity') }}&quot;">
-
-                                    <span class="control-error" v-if="errors.has('quantity')">@{{ errors.first('quantity') }}</span>
+                                    @include ('shop::products.view.configurable-options')
                                 </div>
                             </div>
-                            <div class="col-10 mt-top3-3">
-                                {!! view_render_event('bagisto.shop.products.view.short_description.before', ['product' => $product]) !!}
-                                @include ('shop::products.view.product-add')
+                            <div class="row">
+                                <div class="col-2">
+                                    <div class="quantity control-group" :class="[errors.has('quantity') ? 'has-error' : '']">
+
+                                        <label class="required">{{ __('shop::app.products.quantity') }}</label>
+
+                                        <input name="quantity" class="control" value="1" v-validate="'required|numeric|min_value:1'" style="width: 60px;" data-vv-as="&quot;{{ __('shop::app.products.quantity') }}&quot;">
+
+                                        <span class="control-error" v-if="errors.has('quantity')">@{{ errors.first('quantity') }}</span>
+                                    </div>
+                                </div>
+                                <div class="col-10 mt-top3-3">
+                                    {!! view_render_event('bagisto.shop.products.view.short_description.before', ['product' => $product]) !!}
+                                    @include ('shop::products.view.product-add')
+                                </div>
+
                             </div>
+
+                            <div class="description pt-5">
+                                {!! $product->short_description !!}
+                            </div>
+
+                            {!! view_render_event('bagisto.shop.products.view.short_description.after', ['product' => $product]) !!}
+
+
+                            {!! view_render_event('bagisto.shop.products.view.quantity.before', ['product' => $product]) !!}
+
+
+
+                            {!! view_render_event('bagisto.shop.products.view.quantity.after', ['product' => $product]) !!}
 
                         </div>
-
-                        <div class="description pt-5">
-                            {!! $product->short_description !!}
-                        </div>
-
-                        {!! view_render_event('bagisto.shop.products.view.short_description.after', ['product' => $product]) !!}
-
-
-                        {!! view_render_event('bagisto.shop.products.view.quantity.before', ['product' => $product]) !!}
-
-
-
-                        {!! view_render_event('bagisto.shop.products.view.quantity.after', ['product' => $product]) !!}
-
-                        @if ($product->type == 'configurable')
-                            <input type="hidden" value="true" name="is_configurable">
-                        @else
-                            <input type="hidden" value="false" name="is_configurable">
-                        @endif
-
-                        @include ('shop::products.view.configurable-options')
-
-
-                        {!! view_render_event('bagisto.shop.products.view.description.before', ['product' => $product]) !!}
-
-                        <accordian :title="'{{ __('shop::app.products.description') }}'" :active="false">
-                            <div slot="header">
-                                {{ __('shop::app.products.description') }}
-                                <i class="icon expand-icon right"></i>
-                            </div>
-
-                            <div slot="body">
-                                <div class="full-description">
-                                    {!! $product->description !!}
-                                </div>
-                            </div>
-                        </accordian>
-
-                        {!! view_render_event('bagisto.shop.products.view.description.before', ['product' => $product]) !!}
-
-                        @include ('shop::products.view.attributes')
-                        @if ($total = $reviewHelper->getTotalReviews($product))
-                        <accordian :title="'{{ __('shop::app.products.description') }}'" :active="false">
-                            <div slot="header">
-                               Ratings &  Reviews
-                                <i class="icon expand-icon right"></i>
-                            </div>
-
-                            <div slot="body">
-                                <div class="full-description">
-                                    @include ('shop::products.view.reviews')
-                                </div>
-                            </div>
-                        </accordian>
-                            @endif
                     </div>
-                </div>
+                    <div class="row">
+                        <div class="col-6">
+                            <nav>
+                                <div class="nav nav-tabs" id="nav-tab" role="tablist">
+                                    <a class="nav-item nav-link active text-capitalize" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">DETAILS</a>
+                                    @if ($total = $reviewHelper->getTotalReviews($product))
+                                    <a class="nav-item nav-link text-capitalize" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">REVIEWS</a>
+                                    @endif
+                                    <a class="nav-item nav-link text-capitalize" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false">ATTRIBUTES & CONDITIONS </a>
+                                </div>
+                            </nav>
+                            <div class="tab-content" id="nav-tabContent">
+                                <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+                                    {!! view_render_event('bagisto.shop.products.view.description.before', ['product' => $product]) !!}
+                                    <div slot="body" class="pt-5">
+                                        <div class="full-description">
+                                            {!! $product->description !!}
+                                        </div>
+                                    </div>
+                                    {!! view_render_event('bagisto.shop.products.view.description.before', ['product' => $product]) !!}
+                                </div>
+                                <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
+                                    <div slot="body" class="pt-5">
+                                        <div class="full-description">
+                                            @include ('shop::products.view.reviews')
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">
+                                    @include ('shop::products.view.attributes')
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
             </product-view>
         </div>
 
